eb89509e85b3e42d67e647cd7aa31afa78301390
issamm <issam.elhachimi@gmail.com>
2020-03-23 12:46:04 +0100

    home page with angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+        path: 'home',
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    },
+    {
+        path: 'welcome',
+        component: WelcomeComponent
+    },
+
+    { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+    { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
